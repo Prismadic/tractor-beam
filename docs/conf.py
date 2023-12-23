b4ffc45e0eab04ea8b4ca09f5f359badcddc4fcf
DylanAlloy <alloydylan@icloud.com>
2023-12-23 16:56:35 -0500

    📖 feat(release): docs

---
+++ docs/conf.py
@@ -0,0 +1,58 @@
+import os, sys
+sys.path.insert(0, os.path.abspath('../')) #documentation is detected
+sys.path.insert(0, os.path.abspath('../tractor_beam'))
+
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'tractor_beam'
+copyright = '2023, Prismadic, LLC'
+author = 'Prismadic, LLC'
+version = "v0.0.1"
+release = "latest"
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.todo'
+    , 'sphinx.ext.viewcode'
+    , 'sphinx.ext.autodoc'
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+html_theme = 'sphinx_rtd_theme'
+html_show_sphinx = False
+html_show_copyright = True
+html_context = {
+    "display_github": True, # Integrate GitHub
+    "github_user": "Prismadic", # Username
+    "github_repo": "tractor-beam", # Repo name
+    "github_version": "main", # Version
+    "conf_py_path": "/docs/", # Path in the checkout to the docs root
+}
+html_theme_options = {
+    "collapse_navigation" : False
+    , 'prev_next_buttons_location': 'None'
+}
+html_theme_options = {
+    'logo_only': True,
+    'navigation_depth': 5,
+}
+
+github_url = "https://github.com/Prismadic/tractor-beam"
+display_github = True
+html_logo = "../tractor_beam.png"
+pygments_style = 'dracula'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
---
+++ docs/tractor_beam.rst
@@ -0,0 +1,77 @@
+tractor\_beam package
+=====================
+
+Submodules
+----------
+
+tractor\_beam.config module
+---------------------------
+
+.. automodule:: tractor_beam.config
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+tractor\_beam.copier module
+---------------------------
+
+.. automodule:: tractor_beam.copier
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+tractor\_beam.fax module
+------------------------
+
+.. automodule:: tractor_beam.fax
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+tractor\_beam.janitor module
+----------------------------
+
+.. automodule:: tractor_beam.janitor
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+tractor\_beam.receipts module
+-----------------------------
+
+.. automodule:: tractor_beam.receipts
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+tractor\_beam.supplies module
+-----------------------------
+
+.. automodule:: tractor_beam.supplies
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+tractor\_beam.swan module
+-------------------------
+
+.. automodule:: tractor_beam.swan
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+tractor\_beam.teacher module
+----------------------------
+
+.. automodule:: tractor_beam.teacher
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+tractor\_beam.utils module
+--------------------------
+
+.. automodule:: tractor_beam.utils
+   :members:
+   :undoc-members:
+   :show-inheritance:
