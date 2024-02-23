2cfd3866eea45cf96da5c39f67d345549a2e60aa
Peter-Kang <pkang93@gmail.com>
2024-02-22 23:10:10 -0500

    Updated example and added a way to make the on call

--- tractor_beam/laser/repulse.py
+++ tractor_beam/laser/repulse.py
@@ -5,19 +5,29 @@ class RepulsionBeam:
         """
         initializes the object, takes in a copy of the ip of the ship.
         general class to send things out
-        instantly connects to server if configured to
         """
         self.defaultMotherShip = mothershipIP
+        self.charge = Charge(mothershipIP)
         
 
     def __del__(self):
         """
         disconnect from NATs before closing
         """
+        self.charge.off()
 
+    async def Contact(self, category: str = 'no_category', stream: str = 'documents', create: bool = False):
+        """
+        turns on the charge
+        """
+        await self.charge.on(category,stream, create)
 
-    def ToMotherShip(self, fileLocation:str):
+    async def ToMotherShip(self, fileLocation:str):
         """
         Sends a copy of the file to a NATs server somewhere via magnet
+        Pulse to mother ship
         """
-        print("Repulse"+fileLocation)
+        print("Repulse: "+fileLocation)
+        #Get the file
+        #send it over
+        await self.charge.pulse()
