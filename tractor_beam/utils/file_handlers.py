1f1263c15b9f38c66bf76076daa38c0b852d2922
DylanAlloy <alloydylan@icloud.com>
2024-06-24 14:21:42 -0400

    🚧 fix(wip): pdf processor

--- tractor_beam/utils/file_handlers.py
+++ tractor_beam/utils/file_handlers.py
@@ -7,75 +7,20 @@ from tractor_beam.utils.globals import _f
 from bs4 import BeautifulSoup
 import xml.etree.ElementTree as ET
 import chardet
-import re
 
-class MarkupProcessor:
-    def __init__(self, file_path):
-        self.file_path = file_path
-        self.content = self.read()
-
-    def read(self):
-        with open(self.file_path, 'rb') as f:
-            raw_data = f.read()
-            encoding = chardet.detect(raw_data)['encoding'] or 'utf-8'
-            return raw_data.decode(encoding, errors='replace')
-
-    def remove_html_tags(self, text):
-        # Remove HTML tags from the text
-        soup = BeautifulSoup(text, 'html.parser')
-        text_without_tags = soup.get_text(separator=' ')
-        return text_without_tags
-
-    def xml_to_string(self, xml_data):
-        try:
-            root = ET.fromstring(xml_data)
-            result = ""
-            for element in root.iter():
-                if element.text and element.tag:
-                    try:
-                        result += f"{element.tag.split('}')[1]}: {element.text}\n"
-                    except IndexError:
-                        result += f"{element.tag}: {element.text}\n"
-            return self.remove_html_tags(result)
-        except ET.ParseError as e:
-            # Handle or log the parse error
-            _f('warn', f"Error parsing XML: {e}, attempting to remove tags and return raw data.")
-            return self.remove_html_tags(xml_data)
-
-    def process(self):
-        if self.file_path.endswith('.xml'):
-            processed_text = self.xml_to_string(self.content)
-        else:
-            processed_text = self.content
-        return self.remove_html_tags(processed_text)
-
-    def export_to_markdown(self, output_filepath):
-        with open(output_filepath, 'w', encoding='utf-8') as md_file:
-            content = self.process()
-            md_file.write(content)
-
+from marker import convert as marker
+from marker.models import load_all_models
 
 class PDFProcessor:
     def __init__(self, filepath):
         self.filepath = filepath
-        self.pages_text = []  # Initialize pages_text to ensure the attribute always exists
-
-    def read(self):
-        with open(self.filepath, 'rb') as file:
-            reader = PyPDF2.PdfReader(file)
-            self.pages_text = [reader.pages[page_num].extract_text() for page_num in range(len(reader.pages))]
 
     def export_to_markdown(self, output_filepath):
-        if not self.pages_text:
-            self.read()  # Attempt to read the PDF file again
-        
-        if self.pages_text is None:
-            _f("warn", f"cannot export to Markdown. PDF file {self.filepath} was not successfully parsed.")
-            return
-        else:
-            with open(output_filepath, 'w', encoding='utf-8') as md_file:
-                for i, page_text in enumerate(self.pages_text):
-                    md_file.write(f"## Page {i + 1}\n\n{page_text}\n\n")
+        try:
+            model_lst = load_all_models()
+            marker.convert_single_pdf(output_filepath, model_lst=model_lst)
+        except Exception as e:
+            return e
 
 class HTMLProcessor:
     def __init__(self, filepath):
